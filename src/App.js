3158ae1fbae586fc1a112cf991630cdbb20f21e1
lilo <williamcco@gmail.com>
2019-10-10 23:57:33 -0300

    global style and header component

--- src/App.js
+++ src/App.js
@@ -1,12 +1,15 @@
 import React from "react";
 import { BrowserRouter } from "react-router-dom";
 
+import GlobalStyle from "./styles/global";
+import Header from "./components/Header";
 import Routes from "./routes";
 
 function App() {
   return (
     <BrowserRouter>
-      {/* <Header /> */}
+      <GlobalStyle />
+      <Header />
       <Routes />
     </BrowserRouter>
   );
