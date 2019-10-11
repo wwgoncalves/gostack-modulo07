f77b0b2b056cf3a8f174d78cfa36de713b6e5cfe
lilo <williamcco@gmail.com>
2019-10-11 16:21:39 -0300

    styling Home

--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -1,7 +1,113 @@
 import React from "react";
+import { MdAddShoppingCart } from "react-icons/md";
 
-// import { Container } from './styles';
+import { ProductList } from "./styles";
 
 export default function Home() {
-  return <h1>Home</h1>;
+  return (
+    <ProductList>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/12/F27-0466-012/F27-0466-012_detalhe1.jpg?ims=280x280"
+          alt="Polo Royal"
+        />
+        <strong>Cool sneakers</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+            &nbsp;3
+          </div>
+
+          <span>ADD TO CART</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/12/F27-0466-012/F27-0466-012_detalhe1.jpg?ims=280x280"
+          alt="Polo Royal"
+        />
+        <strong>Cool sneakers</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+            &nbsp;3
+          </div>
+
+          <span>ADD TO CART</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/12/F27-0466-012/F27-0466-012_detalhe1.jpg?ims=280x280"
+          alt="Polo Royal"
+        />
+        <strong>Cool sneakers</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+            &nbsp;3
+          </div>
+
+          <span>ADD TO CART</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/12/F27-0466-012/F27-0466-012_detalhe1.jpg?ims=280x280"
+          alt="Polo Royal"
+        />
+        <strong>Cool sneakers</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+            &nbsp;3
+          </div>
+
+          <span>ADD TO CART</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/12/F27-0466-012/F27-0466-012_detalhe1.jpg?ims=280x280"
+          alt="Polo Royal"
+        />
+        <strong>Cool sneakers</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+            &nbsp;3
+          </div>
+
+          <span>ADD TO CART</span>
+        </button>
+      </li>
+      <li>
+        <img
+          src="https://static.netshoes.com.br/produtos/12/F27-0466-012/F27-0466-012_detalhe1.jpg?ims=280x280"
+          alt="Polo Royal"
+        />
+        <strong>Cool sneakers</strong>
+        <span>R$129,90</span>
+
+        <button type="button">
+          <div>
+            <MdAddShoppingCart size={16} color="#fff" />
+            &nbsp;3
+          </div>
+
+          <span>ADD TO CART</span>
+        </button>
+      </li>
+    </ProductList>
+  );
 }
